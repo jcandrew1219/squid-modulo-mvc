7742ca0caf3b05bc9af9b133e352e3b2f9b5b7ec
sqxxdbxy <jcandrew1219@gmail.com>
2023-07-09 18:11:48 -0400

    added model/index to commit

--- models/index.js
+++ models/index.js
@@ -1,7 +1,13 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
-User.hasMany(Project, {
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
@@ -10,4 +16,17 @@ Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment};
